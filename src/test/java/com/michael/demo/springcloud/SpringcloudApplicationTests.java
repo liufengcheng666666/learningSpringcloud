f00b27c8355a13a0df73a2623b8e31665b9e7fc4
liuwei <727360042@qq.com>
2020-04-14 16:03:44 +0800

    init project

---
+++ src/test/java/com/michael/demo/springcloud/SpringcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael.demo.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
