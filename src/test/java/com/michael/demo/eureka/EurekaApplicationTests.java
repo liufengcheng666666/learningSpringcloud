89f110ab2c17bf96b90ccd2d68481a9cabd1334e
liuwei <727360042@qq.com>
2020-04-14 16:27:58 +0800

    init project

---
+++ src/test/java/com/michael/demo/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael.demo.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
