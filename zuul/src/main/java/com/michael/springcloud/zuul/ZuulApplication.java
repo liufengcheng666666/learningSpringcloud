8a6954d91c14217bbed55f5ad27f4024f1ddcf22
liuwei <727360042@qq.com>
2020-04-24 15:55:32 +0800

    配置服务网关zuul

--- zuul/src/main/java/com/michael/springcloud/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/michael/springcloud/zuul/ZuulApplication.java
@@ -2,10 +2,14 @@ package com.michael.springcloud.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class ZuulApplication {
 
     public static void main(String[] args) {
